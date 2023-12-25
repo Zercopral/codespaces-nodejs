4275159fd2ce2c55aca624cd28945f26d052ba8a
Zercopral <80348366+Zercopral@users.noreply.github.com>
2023-12-25 12:47:22 +0000

    Start ChP2 2

---
+++ ChP2:Chat/Server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+const io = require('socket.io')(app.listen(6000))
+
+app.get('/', (request, response) => 
+    response.sendFile(__dirname + '/Client.html'));
+
+app.use(express.static(__dirname + ''));
+
+io.sockets.on('connection', client => console.log('Кто-то присоединился'));
